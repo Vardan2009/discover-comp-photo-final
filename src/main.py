f93857691e3468728416ef3c6927013c582d9049
Vardan2009 <70532109+Vardan2009@users.noreply.github.com>
2026-01-15 15:58:02 +0400

    Formatting and image output

--- src/main.py
+++ src/main.py
@@ -21,35 +21,40 @@ def center_crop(img, crop_width, crop_height):
     
     return cropped_img.copy() 
 
-img = cv2.imread("imgs/TUMO_0.JPG")
+def img_1():
+    img = cv2.imread("imgs/TUMO_0.JPG")
 
-vhs_overlay = cv2.imread("imgs/VHS.avif")
+    vhs_overlay = cv2.imread("imgs/VHS.avif")
 
-bCh = np.dot(img, [1, 0, 0]).astype(np.float32)
-gCh = np.dot(img, [0, 1, 0]).astype(np.float32)
-rCh = np.dot(img, [0, 0, 1]).astype(np.float32)
+    bCh = np.dot(img, [1, 0, 0]).astype(np.float32)
+    gCh = np.dot(img, [0, 1, 0]).astype(np.float32)
+    rCh = np.dot(img, [0, 0, 1]).astype(np.float32)
 
-rIntensity = 0.7
-gIntensity = 0.7
-bIntensity = 0.8
+    rIntensity = 0.7
+    gIntensity = 0.7
+    bIntensity = 0.8
 
-noiseIntensity = 0.3
+    noiseIntensity = 0.3
 
-rChF = rCh * rIntensity + (np.random.random(rCh.shape) - 0.5) * 150
-gChF = gCh * rIntensity + (np.random.random(gCh.shape) - 0.5) * 150
-bChF = bCh * bIntensity + (np.random.random(rCh.shape) - 0.5) * 150
+    rChF = rCh * rIntensity + (np.random.random(rCh.shape) - 0.5) * 150
+    gChF = gCh * rIntensity + (np.random.random(gCh.shape) - 0.5) * 150
+    bChF = bCh * bIntensity + (np.random.random(rCh.shape) - 0.5) * 150
 
-rChF = np.roll(rChF, axis=1, shift=25)
+    rChF = np.roll(rChF, axis=1, shift=25)
 
-resonstructed = cv2.add(np.stack(
-    (
-        np.clip(rChF, 0, 255).astype(np.uint8),
-        np.clip(gChF, 0, 255).astype(np.uint8),
-        np.clip(bChF, 0, 255).astype(np.uint8),
-    ),
-    axis=2
-), (cv2.resize(vhs_overlay, (bCh.shape[1], bCh.shape[0]), interpolation=cv2.INTER_LINEAR)).astype(np.uint8))
+    reconstructed = cv2.add(np.stack(
+        (
+            np.clip(bChF, 0, 255).astype(np.uint8),
+            np.clip(gChF, 0, 255).astype(np.uint8),
+            np.clip(rChF, 0, 255).astype(np.uint8),
+        ),
+        axis=2
+    ), (cv2.resize(vhs_overlay, (bCh.shape[1], bCh.shape[0]), interpolation=cv2.INTER_LINEAR)).astype(np.uint8))
 
-plt.imshow(resonstructed)
-plt.axis('off')
-plt.show()
+    cv2.imwrite("out/VHS1.png", reconstructed)
+
+    cv2.imshow("VHS Effect", reconstructed)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+img_1()
